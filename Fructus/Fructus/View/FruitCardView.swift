eab10d3cb2a4f87ce4388b0e9bd215fa50bd17e1
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2021-04-26 16:05:41 -0700

    Card View

---
+++ Fructus/Fructus/View/FruitCardView.swift
@@ -0,0 +1,36 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Melissa Zellhuber on 26/04/21.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            VStack (spacing: 20){
+                Image("blueberry").resizable().scaledToFit().shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                Text("Blueberry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+            }
+        }
+        .frame(minWidth: 0, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+    }
+}
+
+// MARK: - Preview
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView().previewLayout(.fixed(width: 320, height: 640))
+    }
+}
