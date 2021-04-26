677af0e19768510c65c26afad8d84921d0ca24f6
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2021-04-26 16:05:34 -0700

    Fructus Assets

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Melissa Zellhuber on 26/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
