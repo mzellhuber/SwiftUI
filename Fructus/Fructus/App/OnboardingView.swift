6a3a5602dd45d4fd0b55b7371ed672bb45e3e331
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2021-04-27 07:43:05 -0700

    Created Onboarding view

---
+++ Fructus/Fructus/App/OnboardingView.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Melissa Zellhuber on 27/04/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        TabView {
+            FruitCardView()
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - Preview
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
