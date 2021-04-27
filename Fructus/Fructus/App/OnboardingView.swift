6d4cc3727083b8e01e9c0859ca57edb94f5c75b5
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2021-04-27 09:29:31 -0700

    Using data dynamically

--- Fructus/Fructus/App/OnboardingView.swift
+++ Fructus/Fructus/App/OnboardingView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct OnboardingView: View {
     // MARK: - Properties
+    var fruits: [Fruit] = fruitsData
     
     // MARK: - Body
     var body: some View {
         TabView {
-            ForEach(0..<5) { item in
-                FruitCardView()
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
             }
         }
         .tabViewStyle(PageTabViewStyle())
@@ -25,6 +26,6 @@ struct OnboardingView: View {
 // MARK: - Preview
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
--- Fructus/Fructus/View/FruitCardView.swift
+++ Fructus/Fructus/View/FruitCardView.swift
@@ -9,21 +9,22 @@ import SwiftUI
 
 struct FruitCardView: View {
     // MARK: - Properties
+    var fruit: Fruit
     @State private var isAnimating: Bool = false
     
     // MARK: - Body
     var body: some View {
         ZStack {
             VStack (spacing: 20){
-                Image("blueberry").resizable().scaledToFit().shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                Image(fruit.image).resizable().scaledToFit().shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
                     .scaleEffect(isAnimating ? 1.0 : 0.6)
-                Text("Blueberry")
+                Text(fruit.title)
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
                 
-                Text("Blueberries are sweet, nutritious and wildly popular fruit all over the world.")
+                Text(fruit.headline)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 16)
@@ -39,7 +40,7 @@ struct FruitCardView: View {
             }
         }
         .frame(minWidth: 0, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
         .padding(.horizontal, 20)
     }
@@ -48,6 +49,7 @@ struct FruitCardView: View {
 // MARK: - Preview
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView().previewLayout(.fixed(width: 320, height: 640))
+        FruitCardView(fruit: fruitsData[1])
+            .previewLayout(.fixed(width: 320, height: 640))
     }
 }
