f25ab05b2747253e3f7e21df774c5a53251bd043
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2021-04-27 07:38:09 -0700

    Start button view

---
+++ Fructus/Fructus/View/StartButtonView.swift
@@ -0,0 +1,39 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Melissa Zellhuber on 26/04/21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        Button(action: {
+            print("Exit onboarding")
+        }, label: {
+            HStack(spacing: 8) {
+                Text("Start")
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+            )
+        })
+        .accentColor(.white)
+    }
+}
+
+// MARK: - Preview
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
