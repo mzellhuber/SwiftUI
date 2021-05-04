7b1a2a9e7bb44f18e625437a6353bc237034af94
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2021-05-04 13:24:05 -0700

    Update CodableBundleExtension.swift

--- Africa/Africa/Extension/CodableBundleExtension.swift
+++ Africa/Africa/Extension/CodableBundleExtension.swift
@@ -10,13 +10,24 @@ import Foundation
 extension Bundle {
     func decode(_ file: String) -> [CoverImage] {
         // Locate json file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
         
         // Create property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
         
         // Create decoder
+        let decoder = JSONDecoder()
         
         // Create property for decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle")
+        }
         
         // Return ready-to-use data
+        return loaded
     }
 }
