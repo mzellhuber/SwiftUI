c6e90cb5b0232069d107ff07a5813a48b2192f75
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2021-05-04 13:40:43 -0700

    Decode bundle extension to generics

--- Africa/Africa/Extension/CodableBundleExtension.swift
+++ Africa/Africa/Extension/CodableBundleExtension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [CoverImage] {
+    func decode<T: Codable>(_ file: String) -> T {
         // Locate json file
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle")
@@ -23,7 +23,7 @@ extension Bundle {
         let decoder = JSONDecoder()
         
         // Create property for decoded data
-        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle")
         }
         
